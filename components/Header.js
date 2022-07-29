587a1ef78e10d5fdfe591563c2e0d1130621b7a4
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-07-28 22:42:37 -0400

    Add right side of a Header component, and make Header sticky to the top.

--- components/Header.js
+++ components/Header.js
@@ -1,45 +1,57 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <div
-      className="flex items-center justify-between max-w-6xl
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div
+        className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto
       "
-    >
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="https://cdn2.downdetector.com/static/uploads/logo/Instagram_Logo_Large.png"
-          className="object-contain"
-          layout="fill"
-          alt="logo"
-        />
-      </div>
-      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png"
-          className="object-contain"
-          layout="fill"
-          alt="logo2"
-        />
-      </div>
-
-      {/*Search Form */}
-
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <SearchIcon className="h-5 text-gray-500 " />
+      >
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://cdn2.downdetector.com/static/uploads/logo/Instagram_Logo_Large.png"
+            className="object-contain"
+            layout="fill"
+            alt="logo"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50  pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png"
+            className="object-contain"
+            layout="fill"
+            alt="logo2"
+          />
+        </div>
+
+        {/*Search Form */}
+
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500 " />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50  pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+
+        {/*Right Section of a Header */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <picture>
+            <img
+              src="avatar.jpeg"
+              alt="avatar"
+              className="h-10 rounded-full cursor-pointer"
+            />
+          </picture>
+        </div>
       </div>
-
-      {/*Search Form */}
-
-      <h1>Right Side</h1>
     </div>
   );
 }
