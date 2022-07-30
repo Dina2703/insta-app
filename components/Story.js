2606203247afd275f0a83cac386e45868cb150b8
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-07-29 22:04:21 -0400

    Style story section and install tailwind-scrollbar

--- components/Story.js
+++ components/Story.js
@@ -5,10 +5,14 @@ function Story({ user }) {
   return (
     <div>
       <picture>
-        <img src={img} alt="username" />
+        <img
+          src={img}
+          alt="username"
+          className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out"
+        />
       </picture>
 
-      <p>{username}</p>
+      <p className="text-xs w-14 truncate">{username}</p>
     </div>
   );
 }
